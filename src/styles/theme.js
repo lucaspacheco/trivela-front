4dbbbaccdb44dbe5bee3db426739d2b25926b071
luizeboli <unifelicio@gmail.com>
2020-09-16 17:53:46 -0300

    style: add responsive font sizes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,35 +1,37 @@
-import { createMuiTheme, colors } from '@material-ui/core';
+import { createMuiTheme, colors, responsiveFontSizes } from '@material-ui/core';
 
-export default createMuiTheme({
-  palette: {
-    primary: {
-      light: '#669c25',
-      main: '#415e1e',
-      dark: '#2D4115',
-    },
-  },
-  typography: {
-    htmlFontSize: 10,
-  },
-  overrides: {
-    MuiSvgIcon: {
-      root: {
-        color: colors.grey[600],
+export default responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      primary: {
+        light: '#669c25',
+        main: '#415e1e',
+        dark: '#2D4115',
       },
     },
-    MuiCssBaseline: {
-      '@global': {
-        html: {
-          fontSize: '62.5%',
+    typography: {
+      htmlFontSize: 10,
+    },
+    overrides: {
+      MuiSvgIcon: {
+        root: {
+          color: colors.grey[600],
         },
-        'html, body, #app': {
-          height: '100%',
-        },
-        '*': {
-          margin: 0,
-          padding: 0,
+      },
+      MuiCssBaseline: {
+        '@global': {
+          html: {
+            fontSize: '62.5%',
+          },
+          'html, body, #app': {
+            height: '100%',
+          },
+          '*': {
+            margin: 0,
+            padding: 0,
+          },
         },
       },
     },
-  },
-});
+  }),
+);
