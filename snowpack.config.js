5416999f857955e38ebe4f94df6e8c4e18847c84
luizeboli <unifelicio@gmail.com>
2020-09-16 09:54:34 -0300

    chore: add absolute import

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   extends: '@snowpack/app-scripts-react',
   plugins: ['@snowpack/plugin-optimize'],
+  alias: {
+    assets: './src/assets',
+    components: './src/components',
+    styles: './src/styles',
+  },
 };
