e71eadc9b9879681f51f50fb6a176a5987f0ddf2
luizeboli <unifelicio@gmail.com>
2020-09-21 10:24:54 -0300

    refactor: organize layout page code

--- src/components/AuthPageLayout/DrawerMenu/index.js
+++ src/components/AuthPageLayout/DrawerMenu/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import {
-  Menu as MenuIcon,
-  Person as PersonIcon,
-  SportsSoccer as SportsSoccerIcon,
-  Payment as PaymentIcon,
-  EmojiEvents as EmojiEventsIcon,
-  Today as TodayIcon,
-  ContactSupport as ContactSupportIcon,
-} from '@material-ui/icons';
-
-import {
   makeStyles,
   Divider,
   SwipeableDrawer,
@@ -20,10 +10,13 @@ import {
   ListItemText,
   Tooltip,
 } from '@material-ui/core';
+import { Menu as MenuIcon } from '@material-ui/icons';
+
+import menuItems from './menuItems';
 
 const useStyles = makeStyles((theme) => ({
   list: {
-    '& > div': {
+    '& > div, & > a': {
       padding: `${theme.spacing(1)}px ${theme.spacing(3)}px`,
     },
   },
@@ -38,52 +31,19 @@ const DrawerMenu = () => {
   const handleCloseDrawer = () => setAnchorEl(null);
 
   const DrawerItems = () => (
-    <>
-      <List className={classes.list}>
-        <ListItem button>
-          <ListItemIcon>
-            <PersonIcon />
-          </ListItemIcon>
-          <ListItemText primary="Meus dados cadastrais" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <SportsSoccerIcon />
-          </ListItemIcon>
-          <ListItemText primary="Meus times" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <PaymentIcon />
-          </ListItemIcon>
-          <ListItemText primary="Meus pagamentos" />
-        </ListItem>
-      </List>
-      <Divider />
-      <List className={classes.list}>
-        <ListItem button>
-          <ListItemIcon>
-            <EmojiEventsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Ligas" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <TodayIcon />
-          </ListItemIcon>
-          <ListItemText primary="Minhas ligas" />
-        </ListItem>
-      </List>
-      <Divider />
-      <List className={classes.list}>
-        <ListItem button>
-          <ListItemIcon>
-            <ContactSupportIcon />
-          </ListItemIcon>
-          <ListItemText primary="Fale conosco" />
-        </ListItem>
-      </List>
-    </>
+    <List className={classes.list}>
+      {menuItems.map(({ id, component, title, icon: Icon, divider }) => (
+        <React.Fragment key={id}>
+          <ListItem button component={component}>
+            <ListItemIcon>
+              <Icon />
+            </ListItemIcon>
+            <ListItemText primary={title} />
+          </ListItem>
+          {divider && <Divider />}
+        </React.Fragment>
+      ))}
+    </List>
   );
 
   return (
---
+++ src/components/AuthPageLayout/DrawerMenu/menuItems.js
@@ -0,0 +1,46 @@
+import {
+  Person as PersonIcon,
+  SportsSoccer as SportsSoccerIcon,
+  Payment as PaymentIcon,
+  EmojiEvents as EmojiEventsIcon,
+  Today as TodayIcon,
+  ContactSupport as ContactSupportIcon,
+} from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+
+export default [
+  {
+    id: 1,
+    component: Link,
+    to: '/profile',
+    title: 'Meus dados cadastrais',
+    icon: PersonIcon,
+  },
+  {
+    id: 2,
+    title: 'Meus times',
+    icon: SportsSoccerIcon,
+  },
+  {
+    id: 3,
+    title: 'Meus pagamentos',
+    icon: PaymentIcon,
+    divider: true,
+  },
+  {
+    id: 4,
+    title: 'Ligas',
+    icon: EmojiEventsIcon,
+  },
+  {
+    id: 5,
+    title: 'Minhas ligas',
+    icon: TodayIcon,
+    divider: true,
+  },
+  {
+    id: 6,
+    title: 'Fale conosco',
+    icon: ContactSupportIcon,
+  },
+];
