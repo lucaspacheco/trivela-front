07a1ff17686a8f88b034a90f8b08432457ab5d10
luizeboli <unifelicio@gmail.com>
2020-09-21 15:46:38 -0300

    refactor: union App component in a single file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,35 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useMutation, ReactQueryConfigProvider } from 'react-query';
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
-import { useMutation } from 'react-query';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import UnauthApp from 'components/App/unauthApp';
-import AuthApp from 'components/App/authApp';
-import Notification from 'components/Notification';
+import Notifications, { useNotify } from 'components/Notification';
 import FullSpinner from 'components/FullSpinner';
+import PrivateRoute from 'components/PrivateRoute';
+import HomePage from 'pages/home';
+import LoginPage from 'pages/login';
+import ProfilePage from 'pages/profile';
+import MyTeamsPage from 'pages/myTeams';
 import theme from 'styles/theme';
 import api from 'services/api';
-import useStore from './store';
+import useAppStore from './store';
+
+const queryConfig = {
+  queries: {
+    refetchOnWindowFocus: false,
+  },
+};
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const notify = useNotify();
   const {
     userInfo: { token },
-    isAuthenticated,
     login,
     logout,
-  } = useStore();
+  } = useAppStore();
 
-  const [checkToken, { isLoading }] = useMutation(
+  const [checkToken, { isLoading: isLoadingMutation }] = useMutation(
     () =>
       api.get('/refresh-token', {
         headers: {
@@ -27,11 +37,45 @@ const App = () => {
         },
       }),
     {
-      // TODO: Snackbar
-      onError: () => logout(),
-      onSuccess: ({ data }) => login(data),
+      onError: () => {
+        logout();
+        setIsLoading(false);
+      },
+      onSuccess: ({ data }) => {
+        login(data);
+        setIsLoading(false);
+      },
     },
   );
+
+  useEffect(() => {
+    const interceptor = api.interceptors.response.use(undefined, (error) => {
+      if (error.response.status === 401) {
+        notify({
+          type: 'error',
+          message: 'Sessão expirada, autentique-se novamente',
+        });
+        logout();
+      }
+      // eslint-disable-next-line no-param-reassign
+      error.originalMessage = error.message;
+      Object.defineProperty(error, 'message', {
+        get() {
+          if (!error.response) {
+            return error.originalMessage;
+          }
+          return (
+            error.response.data.errorMessage || 'Ocorreu um erro inesperado.'
+          );
+        },
+      });
+      return Promise.reject(error);
+    });
+
+    return () => {
+      api.interceptors.response.eject(interceptor);
+    };
+  }, []);
 
   useLayoutEffect(() => {
     (async () => {
@@ -39,17 +83,24 @@ const App = () => {
     })();
   }, []);
 
-  if (isLoading) return <FullSpinner />;
+  if (isLoading || isLoadingMutation) return <FullSpinner />;
 
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Notification />
+    <ReactQueryConfigProvider config={queryConfig}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Notifications />
 
-      <Router>
-          <Route component={isAuthenticated ? AuthApp : UnauthApp} />
-      </Router>
-    </MuiThemeProvider>
+        <Router>
+          <Switch>
+            <PrivateRoute exact path="/" component={HomePage} />
+            <PrivateRoute exact path="/profile" component={ProfilePage} />
+            <PrivateRoute exact path="/my-teams" component={MyTeamsPage} />
+            <Route exact path="/login" component={LoginPage} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
+    </ReactQueryConfigProvider>
   );
 };
 
