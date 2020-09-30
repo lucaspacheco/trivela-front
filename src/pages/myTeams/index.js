0ddccc8d53de177b5f89c54c566c9b9d339822da
luizeboli <unifelicio@gmail.com>
2020-09-30 14:14:59 -0300

    refactor: change my teams page to use table component

--- src/pages/myTeams/index.js
+++ src/pages/myTeams/index.js
@@ -1,79 +1,16 @@
 import React from 'react';
-import RT from 'react-super-responsive-table';
 import { Delete as DeleteIcon } from '@material-ui/icons';
-import {
-  IconButton,
-  Tooltip,
-  Typography,
-  Paper,
-  Button,
-} from '@material-ui/core';
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { IconButton, Tooltip, Typography, Button } from '@material-ui/core';
 
 import AuthPageLayout from 'components/AuthPageLayout';
 import RenderImg from 'components/RenderImg';
+import Table from 'components/Table';
 import { useMyTeams } from 'queries/index';
 import useStyles from './styles';
 
 const MyTeams = () => {
   const classes = useStyles();
   const { teams, isFetching } = useMyTeams();
-
-  const Table = (
-    <Paper className={classes.paper} elevation={3}>
-      <RT.Table className={classes.table}>
-        <RT.Thead>
-          <RT.Tr>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                ID
-              </Typography>
-            </RT.Th>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                Time
-              </Typography>
-            </RT.Th>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                Ações
-              </Typography>
-            </RT.Th>
-          </RT.Tr>
-        </RT.Thead>
-        <RT.Tbody>
-          {teams.map((team) => (
-            <RT.Tr key={team.id}>
-              <RT.Td>
-                <Typography variant="body2">{team.id}</Typography>
-              </RT.Td>
-              <RT.Td className={classes.teamName}>
-                <div>
-                  <RenderImg src={team.imageUrl} width={28} height={28} />
-                  <div>
-                    <Typography variant="subtitle1">{team.name}</Typography>
-                    <Typography variant="caption">
-                      Cartoleiro: {team.cartolaName}
-                    </Typography>
-                  </div>
-                </div>
-              </RT.Td>
-              <RT.Td>
-                <Tooltip title="Excluir time">
-                  <IconButton
-                    className={classes.button}
-                    onClick={() => console.log('delete ID:', team.id)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </Tooltip>
-              </RT.Td>
-            </RT.Tr>
-          ))}
-        </RT.Tbody>
-      </RT.Table>
-    </Paper>
-  );
 
   return (
     <AuthPageLayout heading="Meus times" isFetching={isFetching}>
@@ -86,17 +23,43 @@ const MyTeams = () => {
           Adicionar time
         </Button>
       )}
-      {Table}
+      {!isFetching && (
+        <div className={classes.table}>
+          <Table
+            data={teams.map((team) => ({
+              ID: team.id,
+              Time: (
+                <div className={classes.teamName}>
+                  <RenderImg src={team.imageUrl} width={28} height={28} />
+                  <div>
+                    <Typography variant="subtitle1">{team.name}</Typography>
+                    <Typography variant="caption">
+                      Cartoleiro: {team.cartolaName}
+                    </Typography>
+                  </div>
+                </div>
+              ),
+              Ações: (
+                <Tooltip title="Excluir time">
+                  <IconButton
+                    className={classes.button}
+                    onClick={() => console.log('delete ID:', team.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
+              ),
+            }))}
+          />
+        </div>
+      )}
       {!isFetching && !teams.length && (
-        <Typography variant="h6" className={classes.noTeams}>
-          Ops! Não existem times cadastrados.
-          <Typography variant="h6">
-            Que tal
-            <Button variant="contained" color="primary">
-              Adicionar um time
-            </Button>
-            para começar?
-          </Typography>
+        <Typography variant="body1" className={classes.noTeams}>
+          Que tal
+          <Button variant="contained" color="primary">
+            Adicionar um time
+          </Button>
+          para começar?
         </Typography>
       )}
     </AuthPageLayout>
--- src/pages/myTeams/styles.js
+++ src/pages/myTeams/styles.js
@@ -1,44 +1,11 @@
 import { makeStyles } from '@material-ui/core';
 
 export default makeStyles((theme) => ({
-  button: {},
-  paper: {
-    borderRadius: theme.shape.borderRadius,
-    width: '100%',
-  },
   table: {
-    borderCollapse: 'collapse',
-    borderRadius: theme.shape.borderRadius,
-
-    '& th, & tr:not(:last-child)': {
-      borderBottom: `1px solid ${theme.palette.grey[300]}`,
-    },
-
-    '& th, & td': {
-      padding: `${theme.spacing(0.5)}px ${theme.spacing(4)}px`,
-    },
-
+    width: '100%',
     '@media screen and (max-width: 40em)': {
-      '&.responsiveTable': {
-        '& td.pivoted': {
-          paddingLeft: `calc(40% + 10px) !important`,
-        },
-
-        '& tbody tr': {
-          border: 'none',
-          borderBottom: `1px solid ${theme.palette.grey[300]}`,
-
-          '& $button': {
-            marginLeft: theme.spacing(-2),
-          },
-        },
-
-        '& td': {
-          paddingTop: theme.spacing(1.5),
-          paddingBottom: theme.spacing(1),
-        },
-
-        '& $teamName': {
+      '& .responsiveTable': {
+        '& td:nth-of-type(2)': {
           paddingTop: theme.spacing(2.5),
         },
 
@@ -48,21 +15,14 @@ export default makeStyles((theme) => ({
       },
     },
   },
-  title: {
-    textAlign: 'left',
-    fontWeight: 'bold',
-  },
   teamName: {
-    '& > div': {
-      padding: `${theme.spacing(1)}px 0`,
-      display: 'flex',
-      alignItems: 'center',
+    display: 'flex',
+    alignItems: 'center',
 
-      '& > img': {
-        alignSelf: 'flex-start',
-        marginRight: theme.spacing(2),
-        marginTop: theme.spacing(0.5),
-      },
+    '& > img': {
+      alignSelf: 'flex-start',
+      marginRight: theme.spacing(2),
+      marginTop: theme.spacing(0.5),
     },
   },
   addButton: {
@@ -73,12 +33,8 @@ export default makeStyles((theme) => ({
     textAlign: 'center',
     marginTop: theme.spacing(4),
 
-    '& h6': {
-      marginTop: theme.spacing(3),
-
-      '& > button': {
-        margin: `0 ${theme.spacing(1)}px`,
-      },
+    '& > button': {
+      margin: `0 ${theme.spacing(1)}px`,
     },
   },
 }));
