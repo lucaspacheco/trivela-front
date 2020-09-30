a7e1ec38a5c1ef2bc3897a7a0b98ef00c0532e35
luizeboli <unifelicio@gmail.com>
2020-09-30 13:45:44 -0300

    refactor: change my payments page to use table component

--- src/pages/myPayments/index.js
+++ src/pages/myPayments/index.js
@@ -1,86 +1,26 @@
 import React from 'react';
-import RT from 'react-super-responsive-table';
-import { Typography, Paper } from '@material-ui/core';
 
 import AuthPageLayout from 'components/AuthPageLayout';
 import { formatMoney, dateToString } from 'utils/helpers';
 
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import { useMyPayments } from 'queries/index';
-import useStyles from './styles';
+import Table from 'components/Table';
 
 const MyPayments = () => {
-  const classes = useStyles();
   const { payments, isFetching } = useMyPayments();
-
-  const Table = (
-    <Paper className={classes.paper} elevation={3}>
-      <RT.Table className={classes.table}>
-        <RT.Thead>
-          <RT.Tr>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                ID
-              </Typography>
-            </RT.Th>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                Valor
-              </Typography>
-            </RT.Th>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                Processado em
-              </Typography>
-            </RT.Th>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                Método
-              </Typography>
-            </RT.Th>
-            <RT.Th>
-              <Typography variant="h6" className={classes.title}>
-                Status
-              </Typography>
-            </RT.Th>
-          </RT.Tr>
-        </RT.Thead>
-        <RT.Tbody>
-          {payments.map((payment) => (
-            <RT.Tr key={payment.id}>
-              <RT.Td>
-                <Typography variant="body2">{payment.id}</Typography>
-              </RT.Td>
-              <RT.Td className={classes.paymentValue}>
-                <Typography variant="subtitle1">
-                  {formatMoney(payment.value)}
-                </Typography>
-              </RT.Td>
-              <RT.Td>
-                <Typography variant="subtitle1">
-                  {dateToString(payment.processingDate)}
-                </Typography>
-              </RT.Td>
-              <RT.Td>
-                <Typography variant="subtitle1">{payment.method}</Typography>
-              </RT.Td>
-              <RT.Td>
-                <Typography variant="subtitle1">{payment.status}</Typography>
-              </RT.Td>
-            </RT.Tr>
-          ))}
-        </RT.Tbody>
-      </RT.Table>
-    </Paper>
-  );
 
   return (
     <AuthPageLayout heading="Meus pagamentos" isFetching={isFetching}>
-      {Table}
-      {!isFetching && !payments.length && (
-        <Typography variant="h6" className={classes.noPayments}>
-          Não existem pagamentos a serem exibidos.
-        </Typography>
+      {!isFetching && (
+        <Table
+          data={payments.map((item) => ({
+            ID: item.id,
+            Valor: formatMoney(item.value),
+            'Processado em': dateToString(item.processingDate),
+            Método: item.method,
+            Status: item.status,
+          }))}
+        />
       )}
     </AuthPageLayout>
   );
